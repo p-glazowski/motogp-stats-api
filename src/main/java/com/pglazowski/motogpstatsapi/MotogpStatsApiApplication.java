0489cc0914bb358dff2f92b2ac9b22aecbb8b784
Piotr <hunter976@gmail.com>
2026-07-16 18:13:32 +0200

    Initial upload

---
+++ src/main/java/com/pglazowski/motogpstatsapi/MotogpStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.pglazowski.motogpstatsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotogpStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotogpStatsApiApplication.class, args);
+	}
+
+}
