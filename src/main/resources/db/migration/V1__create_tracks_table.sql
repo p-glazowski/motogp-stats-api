130c6860b4c07b8e6cc640dfe6d07406feac5e92
Piotr <hunter976@gmail.com>
2026-07-16 20:15:49 +0200

    created v1 sql

---
+++ src/main/resources/db/migration/V1__create_tracks_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tracks (
+    id                BIGSERIAL PRIMARY KEY,
+    name              VARCHAR(255) NOT NULL,
+    country           VARCHAR(100) NOT NULL,
+    city              VARCHAR(100),
+    length_km         DOUBLE PRECISION,
+    number_of_turns   INTEGER,
+    race_laps         INTEGER,
+    top_speed_kmh     DOUBLE PRECISION,
+    lap_record_time   VARCHAR(20),
+    lap_record_holder VARCHAR(255),
+    lap_record_year   INTEGER,
+    first_held_year   INTEGER
+);
